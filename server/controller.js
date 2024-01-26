e4dcc752dbe83f2a772eb755b5c21b04a2b99605
Frants <frants.kavalionak@live.com>
2024-01-26 14:08:36 -0500

    feature 1 completed

--- server/controller.js
+++ server/controller.js
@@ -8,6 +8,15 @@ module.exports = {
         let randomCompliment = compliments[randomIndex];
       
         res.status(200).send(randomCompliment);
+    },
+    getFortune: (req, res) => {
+        const fortunes = ["You will be hired by Walmart in California!", "You will be hired by Walmart in Texas!", "You will be hired by Walmart in New Jersey!", "You will be hired by Walmart as a Software Enginner!", "You will be hired by Walmart as a Cybersecurity Professional!"];
+      
+        // choose random future
+        let randomIndex = Math.floor(Math.random() * fortunes.length);
+        let randomFortune = fortunes[randomIndex];
+      
+        res.status(200).send(randomFortune);
     }
 
 }
